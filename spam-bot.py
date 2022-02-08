6245856a7f27c6df7ca6df1dc27fafc54d71194d
xampi.exe <77898068+xavixampi@users.noreply.github.com>
2022-02-08 18:08:14 +0100

    Update spam-bot.py

--- spam-bot.py
+++ spam-bot.py
@@ -5,6 +5,6 @@ from time import sleep
 web.open('https://web.whatsapp.com/send?phone=')#Introducir numero de telefono despues de = 
 sleep(10)
 
-for i in range(100)#Numero de veces que se va a enviar el mensaje:
+for i in range(100):#Numero de veces que se va a enviar el mensaje
     pyautogui.typewrite('Hola buenas, este mensaje ha sido enviado con un bot')
     pyautogui.press('enter')
